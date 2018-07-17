dcd72b334e6ef59e614be38bfe53b61db3ba9827
Joe <jsmonet@gmail.com>
2018-07-17 14:28:50 -0700

    added an all local option. I don\'t like just feeding a blind function like this, but it works. might ref later

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 var (
 	port = flag.IntP("port", "p", 22, "which port")
 	verbose = flag.BoolP("verbose", "v", false, "include the process name in a pretty formatted string?") // simply adding the -v flag with no arguments makes this true
+	allLocal = flag.BoolP("all", "a", false, "return all locally-served ports, their pids, and process names. Note: overrides -p")
 )
 
 func main() {
@@ -18,17 +19,20 @@ func main() {
 
 	flag.Parse()
 
-	// retrieve PID. This is the entire point of the program
-	pidFromPort := findPidfromPort(*port)
-	response := fmt.Sprint(pidFromPort) // just stringing for type consistency below
+	if *allLocal {
+		getLocalPnP()
+	} else {
+		// retrieve PID. This is the entire point of the program
+		pidFromPort := findPidfromPort(*port)
+		response := fmt.Sprint(pidFromPort) // just stringing for type consistency below
 
-	if *verbose {
-		procName := nameFromPid(pidFromPort)
-		response = fmt.Sprintf("%v - %v", pidFromPort, procName)
+		if *verbose {
+			procName := nameFromPid(pidFromPort)
+			response = fmt.Sprintf("%v - %v", pidFromPort, procName)
+		}
+
+		fmt.Println(response)
 	}
-
-	fmt.Println(response)
-
 }
 
 func findPidfromPort (port int) (pid int32) {
@@ -43,6 +47,19 @@ func findPidfromPort (port int) (pid int32) {
 		}
 	}
 	return
+}
+
+func getLocalPnP () {
+	a, _ := net.Connections("tcp")
+
+	for _, datas := range a {
+		switch datas.Laddr.IP {
+		case "::1", "::", ":::", "0.0.0.0", "127.0.0.1":
+			name := nameFromPid(datas.Pid)
+			fmt.Printf("PID: %v Name: %v Port: %v Local: %v\n", datas.Pid, name, datas.Laddr.Port, datas.Laddr.IP)
+		}
+	}
+
 }
 
 func nameFromPid (pid int32) (name string) {
